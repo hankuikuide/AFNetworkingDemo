bf2d4618bda34bfa97b31aceb0e1170f06b2f7f8
tomson <hankuikuide@163.com>
2015-04-20 20:38:32 +0800

    Initial Commit

---
+++ AFNetWorkingDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFNetWorkingDemo
+//
+//  Created by Tomson on 15-4-20.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFNetWorkingDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AFNetWorkingDemo
+//
+//  Created by Tomson on 15-4-20.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
