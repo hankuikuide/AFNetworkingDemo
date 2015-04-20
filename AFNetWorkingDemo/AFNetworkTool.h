01418014f3bb674397502bcca0dc7c44caf80662
tomson <hankuikuide@163.com>
2015-04-20 20:50:07 +0800

    add AFNetworkTool class

---
+++ AFNetWorkingDemo/AFNetworkTool.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworkTool.h
+//  AFNetWorkingDemo
+//
+//  Created by Tomson on 15-4-20.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFNetworkTool : NSObject
+
+@end
---
+++ AFNetWorkingDemo/AFNetworkTool.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworkTool.m
+//  AFNetWorkingDemo
+//
+//  Created by Tomson on 15-4-20.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import "AFNetworkTool.h"
+
+@implementation AFNetworkTool
+
+@end
