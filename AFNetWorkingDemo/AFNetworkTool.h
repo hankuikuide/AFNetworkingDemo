ec2c8c583c067f9a5c02ff04698e6c9a6ca4375f
tomson <hankuikuide@163.com>
2015-04-20 22:20:14 +0800

    Finish AFNetWorkingTools

--- AFNetWorkingDemo/AFNetworkTool.h
+++ AFNetWorkingDemo/AFNetworkTool.h
@@ -7,7 +7,84 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AFNetworking.h"
 
 @interface AFNetworkTool : NSObject
 
+//检测网络状态
++ (void)netWorkStatus;
+
+/**
+ *JSON方式获取数据
+ *urlStr:获取数据的url地址
+ */
++ (void)JSONDataWithUrl:(NSString *)url success:(void(^)(id json))success fail:(void(^)())fail;
+
+/**
+ *JSON方式获取数据
+ *url:获取数据的url地址
+ */
+
++ (void)XMLDataWithUrl:(NSString *)url success:(void(^)(id json))success fail:(void(^)())fail;
+
+/**
+ *JSON方式post提交数据
+ *url:服务器地址
+ *parameters:提交的内容参数
+ */
++ (void)postJSONWithUrl:(NSString *)url parameters:(id)parameters success:(void (^)(id responseObject))success fail:(void (^)())fail;
+
+/**
+ *Session下载文件
+ *url:下载文件的url地址
+ */
++ (void)sessionDownloadWithUrl:(NSString *)url success:(void (^)(NSURL *fileUrl))success fail:(void (^)())fail;
+
+/**
+ *文件上传, 自己定义文件名
+ *url:     需要上传的服务器url
+ *fileUrl: 需要上传的本地文件url
+ *fileName:文件在服务器上以什么名字保存
+ *fileType:文件类型
+ */
++ (void)postUploadWithUrl:(NSString *)url fileUrl:(NSURL *)fileUrl fileName:(NSString *)fileName fileType:(NSString *)fileType success:(void (^)(id responseObject))success fail:(void (^)())fail;
+
+
+/*
+ *文件上传, 文件名由服务器端决定
+ *url:     需要上传的服务器地址
+ *fileUrl: 需要上传 的本地文件url
+ */
++ (void)postUploadWithUrl:(NSString *)url fileUrl:(NSURL *)fileUrl success:(void (^)(id))success fail:(void (^)())fail;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 @end
--- AFNetWorkingDemo/AFNetworkTool.m
+++ AFNetWorkingDemo/AFNetworkTool.m
@@ -10,4 +10,134 @@
 
 @implementation AFNetworkTool
 
++ (void)netWorkStatus {
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        NSLog(@"%d", status);
+    }];
+}
+
++ (void)JSONDataWithUrl:(NSString *)url success:(void (^)(id))success fail:(void (^)())fail {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *dict = @{@"format" : @"json"};
+    [manager GET:url parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject){
+        if (success) {
+            success(responseObject);
+        }
+    
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+        NSLog(@"%@",error);
+        if (fail) {
+            fail();
+        }
+    }];
+}
+
++ (void)XMLDataWithUrl:(NSString *)url success:(void (^)(id))success fail:(void (^)())fail {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFXMLParserResponseSerializer serializer];
+    NSDictionary *dict = @{@"format" : @"xml"};
+    [manager GET:url parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject){
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+        NSLog(@"%@", error);
+        if (fail) {
+            fail();
+        }
+    }];
+}
+
++ (void)postJSONWithUrl:(NSString *)url parameters:(id)parameters success:(void (^)(id))success fail:(void (^)())fail {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager POST:url parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+        NSLog(@"%@",error);
+        if (fail) {
+            fail();
+        }
+    }];
+}
+
++ (void)sessionDownloadWithUrl:(NSString *)url success:(void (^)(NSURL *))success fail:(void (^)())fail {
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:config];
+    NSString *urlString = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSURL *urlstr = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:urlstr];
+    NSURLSessionDownloadTask *task = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSString *cacheDir = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+        NSString *path = [cacheDir stringByAppendingPathComponent:response.suggestedFilename];
+        NSURL *fileUrl = [NSURL fileURLWithPath:path];
+        
+        if (success) {
+            success(fileUrl);
+        }
+        return fileUrl;
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"%@ %@", filePath,error);
+        if (fail) {
+            fail();
+        }
+    }];
+    [task resume];
+}
+
++ (void)postUploadWithUrl:(NSString *)url fileUrl:(NSURL *)fileUrl fileName:(NSString *)fileName fileType:(NSString *)fileType success:(void (^)(id))success fail:(void (^)())fail {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager POST:url parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"头像1.png" withExtension:nil];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+        NSString *fileName = [formatter stringFromDate:[NSDate date]];
+        [formData appendPartWithFileURL:fileURL name:@"uploadFile" fileName:fileName mimeType:fileType error:NULL];
+        
+    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (fail) {
+            fail();
+        }
+    }];
+}
+
+
++ (void)postUploadWithUrl:(NSString *)url fileUrl:(NSURL *)fileUrl success:(void (^)(id))success fail:(void (^)())fail {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager POST:url parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>formData) {
+        NSURL *fileURL = [[NSBundle mainBundle] URLForResource:@"头像1.png" withExtension:nil];
+        [formData appendPartWithFileURL:fileUrl name:@"uploadFile" error:NULL];
+        
+    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *result = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"完成 %@", result);
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"错误 %@", error.localizedDescription);
+        if (fail) {
+            fail();
+        }
+    }];
+}
+
+
+
+
+
+
+
+
+
 @end
